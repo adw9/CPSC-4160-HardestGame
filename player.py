b81db0c8d506de2c92dfdb3a225307065d206f1d
adw9 <adw9@clemson.edu>
2023-03-30 00:06:46 -0400

    remove unnecessary functions from player.py

--- player.py
+++ player.py
@@ -3,11 +3,10 @@ import pygame
 
 class Player():
     rectColor = (0,250, 0)
-    rectSize = rectWidth, rectHeight = 50, 100
+    rectSize = rectWidth, rectHeight = 50, 50
     rectPos = rectX, rectY = 0, 0
     rectSpeed = 0
 
-    #I want a solution to barrers outside of this
     SCREEN_HEIGHT = 0
 
     gameRect = pygame.Rect(rectX, rectY, rectWidth, rectHeight)
@@ -17,12 +16,6 @@ class Player():
         self.rectY = 250
         self.rectSpeed = .5
         self.SCREEN_HEIGHT = 400
-        self.gameRect = pygame.Rect(self.rectX, self.rectY, self.rectWidth, self.rectHeight)
-    #X is unecesary as X never leaves the wall for Pong, but is kept for future adaptability.
-
-    #this function does nothing except move this paddle to the right side of the map
-    def setPlayer2(self):
-        self.rectX = 650
         self.gameRect = pygame.Rect(self.rectX, self.rectY, self.rectWidth, self.rectHeight)
 
 
